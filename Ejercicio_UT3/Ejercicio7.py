f3c06864a1ee58d1e51b69cd60a416fe688d4685
AlvaroCJ01 <alvcorjim@alu.edu.gva.es>
2025-10-24 11:45:45 +0200

    Ej7_actualizacion

--- Ejercicio_UT3/Ejercicio7.py
+++ Ejercicio_UT3/Ejercicio7.py
@@ -1,4 +1,11 @@
 a=float(input("Introduzca la base: "))
 b=float(input("Introduzca la potencia: "))
 
-print(f"{a**b}")
+if b > 0:
+    print(a**b)
+elif b == 0:
+    print("1")
+elif b < 0:
+    print(a**b)
+else:
+    print("Ha ocurrido un error inesperado.")
